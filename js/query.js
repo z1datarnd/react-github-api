5903e67cbcd96feee74ece96b6cf2ac8b8691bb1
Khomyoeung.vet <khamyoeung.vet778@gmail.com>
2021-02-09 16:56:53 +0700

    update project

--- js/query.js
+++ js/query.js
@@ -1,11 +1,9 @@
-
-function getUrl() {
+ function getUrl() {
     var url = 'https://api.github.com/users/data';
     return url;
-}
-
+ }
  $(document).ready(function () {
-    $('#search').keyup(function(){
+    $('#search').on('click', function(){
         $('#result').html('');
         var searchField = $('#search').val();
         var expression = new RegExp(searchField, "i");
@@ -14,15 +12,23 @@ function getUrl() {
             url:getUrl(),
             success : function (data) {
             var result = "";
-                result +=`
-             <p> Name :${data.login}<p>
-             <p> Follower: ${data.followers}</p>
-             <p> Following: ${data.following}</p>
-             <p> Created at: ${data.created_at}</p>
-             <p>Updated at: ${data.updated_at}</p>
-            `;
-            $('#result').append(result);
-            }
-        });
+            result +=`
+            <div class="container">
+            <div class="col-1">
+                <p> Name :${data.login}<p>
+                <p> Follower: ${data.followers}</p>
+                <p> Following: ${data.following}</p>
+                <p> Created at: ${data.created_at}</p>
+                <p> Updated at: ${data.updated_at}</p>
+                <a href="${data.repos_url}">Repos Url</a></div>
+            <div class="col-2">
+                <p> Public repos ${data.public_repos}</p>
+                <a href="${data.repos_url}">Received events</a></div>
+            </div>
+            </div>
+           `;
+        $('#result').append(result);
+        }
+      });
     })
  });
