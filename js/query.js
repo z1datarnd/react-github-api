89eabf8ed6edd58284896cfd24af345ff67756e1
Khomyoeung.vet <khamyoeung.vet778@gmail.com>
2021-02-16 17:02:27 +0700

    test project

--- js/query.js
+++ js/query.js
@@ -1,2 +1,15 @@
+$(document).ready(function () {
+    $(".follower").click(function(){
+        $('#Result').text("Foffower");
+      });
 
+      $(".following").click(function(){
+        $('#Result').text("Following");
+      });
+
+      $(".Repos").click(function(){
+        $('#Result').text("Public Repos");
+      });
+      
+});
  
