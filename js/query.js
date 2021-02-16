112d1cae64fe3c664f4815e0b9676ee5f72479b1
Khomyoeung.vet <khamyoeung.vet778@gmail.com>
2021-02-16 14:45:13 +0700

    fixed project

--- js/query.js
+++ js/query.js
@@ -1,27 +1,2 @@
 
- function getUrlApi(username) {
-    var url = 'https://api.github.com/users/'+ username +'/followers';
-    return url;
-    }
-    $(document).ready(function () {
-    $('#flw').on('click', function(){
-    $(this).data("id")
-    $('#follower').html('');
-    $.ajax({
-        datatype: 'json',
-        url:getUrlApi(),
-        success : function(data) {
-            var total = "";
-            data.forEach(element => {
-                console.log(element);
-                total +=`
-                <img src="${element.avatar_url}" class="avatar">
-                <p class="follower-name">${element.login}</p>
-                `;
-                $('#follower').append(total);
-            });
-        }
-    });
-  });
-});
  
