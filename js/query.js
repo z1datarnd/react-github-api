0464a6d59b56cc7c571080a8fe0db46233763a37
Ngounse71 <73209949+Ngounse71@users.noreply.github.com>
2021-02-22 13:05:45 +0700

    static

--- js/query.js
+++ js/query.js
@@ -2,10 +2,30 @@ $(document).ready(function () {
     $(".clickSearch").click(function(){
        alert('Hello');
       });
-    $(".clickFollower").click(function(){
-        alert('Hello');
-       });
-      
-      
+    
 });
- 
+function getUrlFollower(username){
+    var url = 'https://api.github.com/users/'+ username + '/followers';
+    return url;
+}
+$(document).ready(function(){
+    myFollower();
+    myRepos();
+});
+
+function myFollower(){
+    $('#follower').on('click',function(){
+        $.ajax({
+            datatype:'json',
+            url:getUrlFollower(),
+            success : getFollower,
+        });
+    });
+}
+function myFunction(username) {
+    var url = 'https://avatars.githubusercontent.com/u/2144138?v=4'+ username + '/followers';
+
+    return url;``
+} 
+document.getElementById(demo);
+
